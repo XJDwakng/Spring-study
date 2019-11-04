d0fe7612639cf4295dc24bf174631444390edc9b
XJDwkang <1137397100@qq.com>
2019-11-04 21:05:47 +0800

    删除误提交的

--- src/main/java/com/kangdw/conditional/WindowsCond.java
+++ src/main/java/com/kangdw/conditional/WindowsCond.java
@@ -1,6 +1,5 @@
 package com.kangdw.conditional;
 
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
